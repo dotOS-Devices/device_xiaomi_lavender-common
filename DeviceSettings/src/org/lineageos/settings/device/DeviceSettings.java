7693ba76f4a74c1ba065ff1fd4d7cf91dfe8e6d2
etahamad <etahamad@icloud.com>
2021-02-04 07:45:43 +0200

    sdm660-common: DeviceSettings: Drop thermal
    It's just...useless.

--- DeviceSettings/src/org/lineageos/settings/device/DeviceSettings.java
+++ DeviceSettings/src/org/lineageos/settings/device/DeviceSettings.java
@@ -62,15 +62,10 @@ public class DeviceSettings extends PreferenceFragment implements
     private static final String PREF_DEVICE_DOZE = "device_doze";
     private static final String PREF_DEVICE_KCAL = "device_kcal";
 
-    public static final String PREF_THERMAL = "thermal";
-    public static final String THERMAL_PATH = "/sys/devices/virtual/thermal/thermal_message/sconfig";
-
     private static final String DEVICE_DOZE_PACKAGE_NAME = "com.advanced.settings.doze";
 
     private static final String DEVICE_JASON_PACKAGE_NAME = "org.lineageos.settings.devicex";
     private static final String PREF_DEVICE_JASON = "device_jason";
-
-    private SecureSettingListPreference mTHERMAL;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
@@ -117,11 +112,6 @@ public class DeviceSettings extends PreferenceFragment implements
             return true;
         });
 
-        mTHERMAL = (SecureSettingListPreference) findPreference(PREF_THERMAL);
-        mTHERMAL.setValue(FileUtils.getValue(THERMAL_PATH));
-        mTHERMAL.setSummary(mTHERMAL.getEntry());
-        mTHERMAL.setOnPreferenceChangeListener(this);
-
     }
 
     @Override
@@ -143,12 +133,6 @@ public class DeviceSettings extends PreferenceFragment implements
 
             case PREF_MIC_GAIN:
                 FileUtils.setValue(MIC_GAIN_PATH, (int) value);
-                break;
-
-            case PREF_THERMAL:
-                mTHERMAL.setValue((String) value);
-                mTHERMAL.setSummary(mTHERMAL.getEntry());
-                FileUtils.setValue(THERMAL_PATH, (String) value);
                 break;
 
             case PREF_KEY_FPS_INFO:
