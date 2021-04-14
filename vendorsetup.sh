5b63dd5dcf46f6a306017ca42393d9d9d28a838c
chrisw444 <wandersonrodriguesf1@gmail.com>
2021-04-14 10:02:50 -0300

    sdm660-common: use vendorsetup.sh
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,8 @@
+# export
+export SKIP_ABI_CHECKS=true
+
+# clone
+git clone https://github.com/ChrisW444/android_vendor_codeaurora_telephony vendor/codeaurora/telephony
+git clone https://github.com/SDM660-Project/android_hardware_qcom_display -b 4.4 hardware/qcom-caf/msm8998/display
+git clone https://github.com/SDM660-Project/android_hardware_qcom_audio -b 4.4 hardware/qcom-caf/msm8998/audio
+git clone https://github.com/SDM660-Project/android_hardware_qcom_media -b 4.4 hardware/qcom-caf/msm8998/media
