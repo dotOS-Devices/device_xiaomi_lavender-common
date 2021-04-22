ab08388b6ff40041565323da1ec835bf6e265679
Flex1911 <dedsa2002@gmail.com>
2021-04-22 13:52:33 +0700

    sdm660-common : Disable sched_boost on post_boot

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -1999,6 +1999,9 @@ case "$target" in
             # Enable low power modes
             echo 0 > /sys/module/lpm_levels/parameters/sleep_disabled
 
+            # Turn off scheduler boost at the end
+            echo 0 > /proc/sys/kernel/sched_boost
+
             # Set Memory parameters
             configure_memory_parameters
 
